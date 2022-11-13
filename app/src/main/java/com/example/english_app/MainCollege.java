d6d473e2d6ad485b1c1289caf417e704d008198d
PhoebeJChang <phoebechang00@gmail.com>
2022-11-14 00:21:23 +0800

    tool bar xmlMainCollege(xml&java)

--- app/src/main/java/com/example/english_app/MainCollege.java
+++ app/src/main/java/com/example/english_app/MainCollege.java
@@ -1,6 +1,8 @@
 package com.example.english_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 public class MainCollege extends AppCompatActivity {
@@ -9,6 +11,12 @@ public class MainCollege extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_college);
+
+        //記得這裡的Toolbar 使用的是androidx.appcompat.widget.Toolbar，不可以選錯宣告(by P)
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
+
+
 
 }
