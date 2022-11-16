5a6aa8f74d1cf4bd8c8498142989c52096d3c4dc
鍾馥羽 <fuyuchung@gmail.com>
2022-11-16 23:26:18 +0800

    密碼加密 hash salt

---
+++ app/src/main/java/com/example/english_app/Encryption.java
@@ -0,0 +1,50 @@
+package com.example.english_app;
+
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+public class Encryption {
+    public static String generatedSalt() {
+        byte[] salt = new byte[8];
+        new Random().nextBytes(salt);
+        String generatedSalt = new String(salt, Charset.forName("UTF-8"));
+        System.out.println(generatedSalt);
+        return generatedSalt;
+    }
+    public static String sha1(String clearString) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+            messageDigest.update(clearString.getBytes("UTF-8"));
+            byte[] bytes = messageDigest.digest();
+            StringBuilder buffer = new StringBuilder();
+            for (byte b : bytes) {
+                buffer.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
+            }
+            return buffer.toString();
+        }
+        catch (Exception ignored) {
+            ignored.printStackTrace();
+            return null;
+        }
+    }
+
+    public static String md5(String s) {
+        try {
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < messageDigest.length; i++)
+                hexString.append(Integer.toHexString(0xFF & messageDigest[i]));
+
+            return hexString.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
