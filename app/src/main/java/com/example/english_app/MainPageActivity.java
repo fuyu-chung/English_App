b72df9698d5357038108d40edffc533650cff2f3
PhoebeJChang <phoebechang00@gmail.com>
2022-11-18 21:11:48 +0800

    fix buggggg

--- app/src/main/java/com/example/english_app/MainPageActivity.java
+++ app/src/main/java/com/example/english_app/MainPageActivity.java
@@ -1,6 +1,7 @@
 package com.example.english_app;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
@@ -8,14 +9,16 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class MainPageActivity extends AppCompatActivity {
+public class MainPageActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     //number of selectedTab. 我們有五個頁面所以 values 介於1-5之簡，default value = 1
     private int selectedTab = 1;
@@ -42,9 +45,12 @@ public class MainPageActivity extends AppCompatActivity {
         toolbar.setNavigationIcon(R.drawable.menu_icon);
 
         /*------------------------------NAVIGATION DRAWER MENU -----------------------------*/
+        navigationView.bringToFront();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
 
 
         /*------------------------------BOTTOM BAR!!!!!-------------------------------------*/
@@ -324,5 +330,23 @@ public class MainPageActivity extends AppCompatActivity {
 
     }
 
+    /*-------------------------To avoid closing the app on Back pressed---------------------------*/
+    @Override
+    public void onBackPressed() {
 
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    /*----------------------------       Set navigation item       -------------------------------*/
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
