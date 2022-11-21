832bfff95ea8218f944e1fafcd35a0063c9018c3
PhoebeJChang <phoebechang00@gmail.com>
2022-11-21 21:27:39 +0800

    password changed

--- app/src/main/java/com/example/english_app/MainPageActivity.java
+++ app/src/main/java/com/example/english_app/MainPageActivity.java
@@ -396,6 +396,11 @@ public class MainPageActivity extends AppCompatActivity implements NavigationVie
                 Intent intent = new Intent(this, UserProfileActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.nav_passwordChange:
+                Intent intent2 = new Intent(this, ChangePassword.class);
+                startActivity(intent2);
+                break;
+
         }
 
         drawerLayout.closeDrawer(GravityCompat.START); //if any item selected, close drawer
