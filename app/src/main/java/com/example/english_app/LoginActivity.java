dd334bf9bcbfed21e5b733192426501c2e724322
PhoebeJChang <phoebechang00@gmail.com>
2022-11-27 02:59:20 +0800

    小bug

--- app/src/main/java/com/example/english_app/LoginActivity.java
+++ app/src/main/java/com/example/english_app/LoginActivity.java
@@ -94,7 +94,7 @@ public class LoginActivity extends AppCompatActivity {
         registerBtn.setOnClickListener(v -> {
             ExecutorService executor = Executors.newSingleThreadExecutor(); // 建立新的thread
             executor.execute(() -> {
-                Intent intent = new Intent(this, ChangePasswordActivity.class);
+                Intent intent = new Intent(this, RegisterActivity.class);
                 startActivity(intent);
             });
         });
@@ -103,7 +103,7 @@ public class LoginActivity extends AppCompatActivity {
         forgetBtn.setOnClickListener(v -> {
             ExecutorService executor = Executors.newSingleThreadExecutor(); // 建立新的thread
             executor.execute(() -> {
-                Intent intent = new Intent(this, RegisterActivity.class); //
+                Intent intent = new Intent(this, ForgetPasswordActivity.class); //
                 startActivity(intent);
             });
         });
