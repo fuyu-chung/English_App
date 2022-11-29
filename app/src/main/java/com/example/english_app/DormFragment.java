250b081fde2e7945689fcf526d95cdde6b7009c8
PhoebeJChang <phoebechang00@gmail.com>
2022-11-29 16:31:27 +0800

    OAOAOAOA

--- app/src/main/java/com/example/english_app/DormFragment.java
+++ app/src/main/java/com/example/english_app/DormFragment.java
@@ -6,16 +6,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DormFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class DormFragment extends Fragment {
 
 
@@ -25,42 +22,42 @@ public class DormFragment extends Fragment {
     private ImageButton imgbtnCal, imgbtnProfile, imgbtnTask, imgbtnClock, imgbtnFriend, imgbtnNote, imgbtnColl;
 
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+//    // TODO: Rename parameter arguments, choose names that match
+//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//    private static final String ARG_PARAM1 = "param1";
+//    private static final String ARG_PARAM2 = "param2";
 
     public DormFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DormFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DormFragment newInstance(String param1, String param2) {
-        DormFragment fragment = new DormFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // TODO: Rename and change types of parameters
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    /**
+//     * Use this factory method to create a new instance of
+//     * this fragment using the provided parameters.
+//     *
+//     * @param param1 Parameter 1.
+//     * @param param2 Parameter 2.
+//     * @return A new instance of fragment DormFragment.
+//     */
+//    // TODO: Rename and change types and number of parameters
+//    public static DormFragment newInstance(String param1, String param2) {
+//        DormFragment fragment = new DormFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            // TODO: Rename and change types of parameters
+//            String mParam1 = getArguments().getString(ARG_PARAM1);
+//            String mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -68,27 +65,51 @@ public class DormFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_dorm, container, false);
 
-        //all the variables
-        dormInfo = view.findViewById(R.id.dorm_info);
-        imgbtnCal = view.findViewById(R.id.imgbtn_cal);
-        imgbtnProfile = view.findViewById(R.id.imgbtn_profile);
-        imgbtnTask = view.findViewById(R.id.imgbtn_task);
+        //all the variables of img BTN
+        ExtendedFloatingActionButton dormInfo = view.findViewById(R.id.dorm_info);
+        ImageButton imgbtnCal = view.findViewById(R.id.imgbtn_cal);
+        ImageButton imgbtnProfile = view.findViewById(R.id.imgbtn_profile);
+        ImageButton imgbtnTask = view.findViewById(R.id.imgbtn_task);
+        ImageButton imgbtnClock = view.findViewById(R.id.imgbtn_clock);
+        ImageButton imgbtnFriend = view.findViewById(R.id.imgbtn_friend);
+        ImageButton imgbtnNote = view.findViewById(R.id.imgbtn_note);
+        ImageButton imgbtnColl = view.findViewById(R.id.imgbtn_coll);
+        ImageButton imgbtnWrong = view.findViewById(R.id.imgbtn_wrong);
         imgbtnClock = view.findViewById(R.id.imgbtn_clock);
-        imgbtnFriend = view.findViewById(R.id.imgbtn_friend);
-        imgbtnNote = view.findViewById(R.id.imgbtn_note);
-        imgbtnColl = view.findViewById(R.id.imgbtn_coll);
 
-        //write down all the gone texts
+        //write down all the invisible layout
+        Boolean isFabVisible = false;
+        LinearLayout hintCal = view.findViewById(R.id.hint_cal);
+        LinearLayout hintProfile = view.findViewById(R.id.hint_profile);
+        LinearLayout hintTask = view.findViewById(R.id.hint_task);
+        LinearLayout hintNote = view.findViewById(R.id.hint_note);
+        LinearLayout hintColl = view.findViewById(R.id.hint_coll);
+        LinearLayout hintWrong = view.findViewById(R.id.hint_wrong);
+        LinearLayout hintClock = view.findViewById(R.id.hint_clock);
+        LinearLayout hintFriend = view.findViewById(R.id.hint_friend);
+
+        hintCal.setVisibility(View.INVISIBLE);
+        hintProfile.setVisibility(View.INVISIBLE);
+        hintTask.setVisibility(View.INVISIBLE);
+        hintNote.setVisibility(View.INVISIBLE);
+        hintColl.setVisibility(View.INVISIBLE);
+        hintWrong.setVisibility(View.INVISIBLE);
+        hintClock.setVisibility(View.INVISIBLE);
+        hintFriend.setVisibility(View.INVISIBLE);
 
         //boolean as false
-        isFABVisible = false;
+        isFabVisible = false;
 
-        imgbtnProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), UserProfileActivity.class);
-                startActivity(intent);
-            }
+        //dormInfo first shrink to circle
+        dormInfo.shrink();
+
+        dormInfo.setOnClickListener(v -> {
+
+        });
+
+        imgbtnProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), UserProfileActivity.class);
+            startActivity(intent);
         });
         return view;
     }
