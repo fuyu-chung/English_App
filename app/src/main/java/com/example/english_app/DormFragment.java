1c85d6c3f32b4e6b2c21ce598e9a5500eb14fabe
PhoebeJChang <phoebechang00@gmail.com>
2022-11-30 00:01:10 +0800

    dorm javva

--- app/src/main/java/com/example/english_app/DormFragment.java
+++ app/src/main/java/com/example/english_app/DormFragment.java
@@ -22,42 +22,11 @@ public class DormFragment extends Fragment {
     private ImageButton imgbtnCal, imgbtnProfile, imgbtnTask, imgbtnClock, imgbtnFriend, imgbtnNote, imgbtnColl;
 
 
-//    // TODO: Rename parameter arguments, choose names that match
-//    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-//    private static final String ARG_PARAM1 = "param1";
-//    private static final String ARG_PARAM2 = "param2";
-
     public DormFragment() {
         // Required empty public constructor
     }
 
-//    /**
-//     * Use this factory method to create a new instance of
-//     * this fragment using the provided parameters.
-//     *
-//     * @param param1 Parameter 1.
-//     * @param param2 Parameter 2.
-//     * @return A new instance of fragment DormFragment.
-//     */
-//    // TODO: Rename and change types and number of parameters
-//    public static DormFragment newInstance(String param1, String param2) {
-//        DormFragment fragment = new DormFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-//
-//    @Override
-//    public void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            // TODO: Rename and change types of parameters
-//            String mParam1 = getArguments().getString(ARG_PARAM1);
-//            String mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
-//    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -78,7 +47,7 @@ public class DormFragment extends Fragment {
         imgbtnClock = view.findViewById(R.id.imgbtn_clock);
 
         //write down all the invisible layout
-        Boolean isFabVisible = false;
+        final boolean[] isFabVisible = new boolean[1];
         LinearLayout hintCal = view.findViewById(R.id.hint_cal);
         LinearLayout hintProfile = view.findViewById(R.id.hint_profile);
         LinearLayout hintTask = view.findViewById(R.id.hint_task);
@@ -88,23 +57,41 @@ public class DormFragment extends Fragment {
         LinearLayout hintClock = view.findViewById(R.id.hint_clock);
         LinearLayout hintFriend = view.findViewById(R.id.hint_friend);
 
-        hintCal.setVisibility(View.INVISIBLE);
-        hintProfile.setVisibility(View.INVISIBLE);
-        hintTask.setVisibility(View.INVISIBLE);
-        hintNote.setVisibility(View.INVISIBLE);
-        hintColl.setVisibility(View.INVISIBLE);
-        hintWrong.setVisibility(View.INVISIBLE);
-        hintClock.setVisibility(View.INVISIBLE);
-        hintFriend.setVisibility(View.INVISIBLE);
 
         //boolean as false
-        isFabVisible = false;
+        isFabVisible[0] = false;
 
         //dormInfo first shrink to circle
         dormInfo.shrink();
 
         dormInfo.setOnClickListener(v -> {
+            if(!isFabVisible[0]){
+                hintCal.setVisibility(View.VISIBLE);
+                hintProfile.setVisibility(View.VISIBLE);
+                hintTask.setVisibility(View.VISIBLE);
+                hintNote.setVisibility(View.VISIBLE);
+                hintColl.setVisibility(View.VISIBLE);
+                hintWrong.setVisibility(View.VISIBLE);
+                hintClock.setVisibility(View.VISIBLE);
+                hintFriend.setVisibility(View.VISIBLE);
 
+                dormInfo.extend();
+
+                isFabVisible[0] = true;
+            }else{
+                hintCal.setVisibility(View.INVISIBLE);
+                hintProfile.setVisibility(View.INVISIBLE);
+                hintTask.setVisibility(View.INVISIBLE);
+                hintNote.setVisibility(View.INVISIBLE);
+                hintColl.setVisibility(View.INVISIBLE);
+                hintWrong.setVisibility(View.INVISIBLE);
+                hintClock.setVisibility(View.INVISIBLE);
+                hintFriend.setVisibility(View.INVISIBLE);
+
+                dormInfo.shrink();
+
+                isFabVisible[0] = false;
+            }
         });
 
         imgbtnProfile.setOnClickListener(v -> {
