d6cb3c85a1108cb3159116a887810b8473b5aa46
PhoebeJChang <phoebechang00@gmail.com>
2022-12-05 22:01:54 +0800

    btn連接

---
+++ app/src/main/java/com/example/english_app/colleges/vocab/VocabActivity.java
@@ -0,0 +1,16 @@
+package com.example.english_app.colleges.vocab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.english_app.R;
+
+public class VocabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vocab);
+    }
+}
--- app/src/main/java/com/example/english_app/mainpage_fragments/CollegeFragment.java
+++ app/src/main/java/com/example/english_app/mainpage_fragments/CollegeFragment.java
@@ -1,5 +1,6 @@
 package com.example.english_app.mainpage_fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.transition.AutoTransition;
 import android.transition.TransitionManager;
@@ -14,12 +15,13 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.english_app.R;
+import com.example.english_app.colleges.compete.CompeteActivity;
+import com.example.english_app.colleges.phrase.PhraseActivity;
+import com.example.english_app.colleges.read.ReadingActivity;
+import com.example.english_app.colleges.vocab.VocabActivity;
+import com.google.android.material.button.MaterialButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CollegeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class CollegeFragment extends Fragment {
 
     private TextView detailText01, detailText02, detailText03, detailText04;
@@ -28,46 +30,13 @@ public class CollegeFragment extends Fragment {
     private TextView shrinkText01, shrinkText02, shrinkText03, shrinkText04;
     private ImageView downIcon01, downIcon02, downIcon03, downIcon04;
     private ImageView upIcon01, upIcon02, upIcon03, upIcon04;
+    private MaterialButton vocabBtn, phraseBtn, readingBtn, competeBtn;
 
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     public CollegeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CollegeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CollegeFragment newInstance(String param1, String param2) {
-        CollegeFragment fragment = new CollegeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // TODO: Rename and change types of parameters
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -85,6 +54,7 @@ public class CollegeFragment extends Fragment {
         shrinkText01 = view.findViewById(R.id.shrinkCollegeLayout01Text);
         downIcon01 = view.findViewById(R.id.collegeLayout01DownIcon);
         upIcon01 = view.findViewById(R.id.collegeLayout01UpIcon);
+        vocabBtn = view.findViewById(R.id.vocab_btn);
 
 
         cardView01.setOnClickListener(v -> {
@@ -94,12 +64,14 @@ public class CollegeFragment extends Fragment {
             int var3 = (shrinkText01.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var4 = (downIcon01.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var5 = (upIcon01.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
+            int var6 = (vocabBtn.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             TransitionManager.beginDelayedTransition(relativeLayout01, new AutoTransition());
             detailText01.setVisibility(var);
             extendText01.setVisibility(var2);
             shrinkText01.setVisibility(var3);
             downIcon01.setVisibility(var4);
             upIcon01.setVisibility(var5);
+            vocabBtn.setVisibility(var6);
 
         });
 
@@ -110,6 +82,7 @@ public class CollegeFragment extends Fragment {
         shrinkText02 = view.findViewById(R.id.shrinkCollegeLayout02Text);
         downIcon02 = view.findViewById(R.id.collegeLayout02DownIcon);
         upIcon02 = view.findViewById(R.id.collegeLayout02UpIcon);
+        phraseBtn = view.findViewById(R.id.phrase_btn);
 
         cardView02.setOnClickListener(v -> {
 
@@ -118,12 +91,14 @@ public class CollegeFragment extends Fragment {
             int var3 = (shrinkText02.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var4 = (downIcon02.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var5 = (upIcon02.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
+            int var6 = (phraseBtn.getVisibility() == View.VISIBLE)? View.GONE: View.VISIBLE;
             TransitionManager.beginDelayedTransition(relativeLayout02, new AutoTransition());
             detailText02.setVisibility(var);
             extendText02.setVisibility(var2);
             shrinkText02.setVisibility(var3);
             downIcon02.setVisibility(var4);
             upIcon02.setVisibility(var5);
+            phraseBtn.setVisibility(var6);
         });
 
         CardView cardView03 = view.findViewById(R.id.cardViewReading);
@@ -133,6 +108,7 @@ public class CollegeFragment extends Fragment {
         shrinkText03 = view.findViewById(R.id.shrinkCollegeLayout03Text);
         downIcon03 = view.findViewById(R.id.collegeLayout03DownIcon);
         upIcon03 = view.findViewById(R.id.collegeLayout03UpIcon);
+        readingBtn = view.findViewById(R.id.reading_btn);
 
 
         cardView03.setOnClickListener(v -> {
@@ -142,12 +118,14 @@ public class CollegeFragment extends Fragment {
             int var3 = (shrinkText03.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var4 = (downIcon03.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var5 = (upIcon03.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
+            int var6 = (readingBtn.getVisibility() == View.VISIBLE)? View.GONE: View.VISIBLE;
             TransitionManager.beginDelayedTransition(relativeLayout03, new AutoTransition());
             detailText03.setVisibility(var);
             extendText03.setVisibility(var2);
             shrinkText03.setVisibility(var3);
             downIcon03.setVisibility(var4);
             upIcon03.setVisibility(var5);
+            readingBtn.setVisibility(var6);
 
 
         });
@@ -159,6 +137,7 @@ public class CollegeFragment extends Fragment {
         shrinkText04 = view.findViewById(R.id.shrinkCollegeLayout04Text);
         downIcon04 = view.findViewById(R.id.collegeLayout04DownIcon);
         upIcon04 = view.findViewById(R.id.collegeLayout04UpIcon);
+        competeBtn = view.findViewById(R.id.compete_btn);
 
         cardView04.setOnClickListener(v -> {
 
@@ -167,14 +146,32 @@ public class CollegeFragment extends Fragment {
             int var3 = (shrinkText04.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var4 = (downIcon04.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             int var5 = (upIcon04.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
+            int var6 = (competeBtn.getVisibility() == View.VISIBLE)? View.GONE : View.VISIBLE;
             TransitionManager.beginDelayedTransition(relativeLayout04, new AutoTransition());
             detailText04.setVisibility(var);
             extendText04.setVisibility(var2);
             shrinkText04.setVisibility(var3);
             downIcon04.setVisibility(var4);
             upIcon04.setVisibility(var5);
+            competeBtn.setVisibility(var6);
         });
 
+        vocabBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), VocabActivity.class);
+            startActivity(intent);
+        });
+        phraseBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), PhraseActivity.class);
+            startActivity(intent);
+        });
+        readingBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ReadingActivity.class);
+            startActivity(intent);
+        });
+        competeBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), CompeteActivity.class);
+            startActivity(intent);
+        });
 
         return view;
     }
