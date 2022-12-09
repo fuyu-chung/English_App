bf0abb1dda357e820c269d9bc281cf6288ec7705
PhoebeJChang <phoebechang00@gmail.com>
2022-12-09 21:43:39 +0800

    vocab cat

--- app/src/main/java/com/example/english_app/colleges/vocab/StaticRcvAdapter.java
+++ app/src/main/java/com/example/english_app/colleges/vocab/StaticRcvAdapter.java
@@ -1,4 +1,98 @@
 package com.example.english_app.colleges.vocab;
 
-public class StaticRcvAdapter {
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.english_app.R;
+
+import java.util.ArrayList;
+
+public class StaticRcvAdapter extends RecyclerView.Adapter<StaticRcvAdapter.StaticRcvViewHolder>{
+
+    private ArrayList<StaticRcvModel> catItems;
+    int check_position = 0; //-1為no default，0為第一選項clicked
+    private Context context;
+
+    public StaticRcvAdapter(ArrayList<StaticRcvModel> catItems) {
+        this.context = context;
+        this.catItems = catItems;
+    }
+
+    @NonNull
+    @Override
+    public StaticRcvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.static_rcv_item, parent, false);
+        StaticRcvViewHolder staticRcvViewHolder = new StaticRcvViewHolder(view);
+        return staticRcvViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StaticRcvViewHolder holder, final int position) {
+        StaticRcvModel currentItem = catItems.get(position);
+        holder.vocImage.setImageResource(currentItem.getImage());
+        holder.vocText.setText(currentItem.getText());
+
+//        holder.vocSelectedLayout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                check_position = position;
+//                notifyDataSetChanged();
+//            }
+//        });
+
+        if(check_position == position){
+            holder.vocSelectedLayout.setBackgroundResource(R.drawable.voc_cat_rcv_selected);
+        }else{
+            holder.vocSelectedLayout.setBackgroundResource(R.drawable.voc_cat_rcv_bg);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return catItems.size();
+    }
+
+    public class StaticRcvViewHolder extends RecyclerView.ViewHolder {
+
+        private final TextView vocText;
+        private final ImageView vocImage;
+        private final LinearLayout vocSelectedLayout;
+
+
+        public StaticRcvViewHolder(@NonNull View itemView) {
+            super(itemView);
+            vocImage = itemView.findViewById(R.id.vocCatImage);
+            vocText = itemView.findViewById(R.id.vocCatText);
+            vocSelectedLayout = itemView.findViewById(R.id.voc_rcv_linearlayout);
+
+            vocSelectedLayout.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    checkPosition(getAbsoluteAdapterPosition());
+
+                }
+            });
+
+
+        }
+        private void checkPosition(int adapterPosition){
+            if(adapterPosition == RecyclerView.NO_POSITION){
+                return;
+            }
+            notifyItemChanged(check_position);
+            check_position = adapterPosition;
+            notifyItemChanged(check_position);
+        }
+        
+    }
 }
--- app/src/main/java/com/example/english_app/colleges/vocab/VocabActivity.java
+++ app/src/main/java/com/example/english_app/colleges/vocab/VocabActivity.java
@@ -1,16 +1,35 @@
 package com.example.english_app.colleges.vocab;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.english_app.R;
 
+import java.util.ArrayList;
+
 public class VocabActivity extends AppCompatActivity {
+
+    private RecyclerView rcvVocTitle;
+    private StaticRcvAdapter staticRcvAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vocab);
+
+        ArrayList<StaticRcvModel> catItem = new ArrayList<StaticRcvModel>();
+        catItem.add(new StaticRcvModel(R.drawable.ic_elem, "國小"));
+        catItem.add(new StaticRcvModel(R.drawable.ic_jhs, "國中"));
+        catItem.add(new StaticRcvModel(R.drawable.ic_hs, "高中"));
+        catItem.add(new StaticRcvModel(R.drawable.ic_toeic, "多益"));
+        catItem.add(new StaticRcvModel(R.drawable.ic_toefl, "托福"));
+
+        rcvVocTitle = findViewById(R.id.voc_rcv_cat);
+        staticRcvAdapter = new StaticRcvAdapter(catItem);
+        rcvVocTitle.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+        rcvVocTitle.setAdapter(staticRcvAdapter);
     }
 }
