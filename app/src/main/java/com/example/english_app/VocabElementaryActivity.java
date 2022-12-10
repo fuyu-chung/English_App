ab05ef0a681895d75f1dba216936070be7493d41
PhoebeJChang <phoebechang00@gmail.com>
2022-12-10 08:24:23 +0800

    vocab xml

---
+++ app/src/main/java/com/example/english_app/VocabElementaryActivity.java
@@ -0,0 +1,14 @@
+package com.example.english_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VocabElementaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vocab_elementary);
+    }
+}
