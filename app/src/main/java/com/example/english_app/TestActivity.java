7d5a274f8b9143142c9d6d82b510f0e1e02ee8f8
鍾馥羽 <fuyuchung@gmail.com>
2022-12-12 00:13:25 +0800

    TestActivity

--- app/src/main/java/com/example/english_app/TestActivity.java
+++ app/src/main/java/com/example/english_app/TestActivity.java
@@ -39,7 +39,6 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-
         optionA = findViewById(R.id.optionA);
         optionB = findViewById(R.id.optionB);
         optionC = findViewById(R.id.optionC);
