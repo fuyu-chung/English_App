22138ba639a889c32a11d7f9ba004ac7a9f3b389
鍾馥羽 <fuyuchung@gmail.com>
2022-12-11 21:46:11 +0800

    MyTaskActivity

---
+++ app/src/main/java/com/example/english_app/user_dorm/MyTaskActivity.java
@@ -0,0 +1,16 @@
+package com.example.english_app.user_dorm;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.english_app.R;
+
+public class MyTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_task);
+    }
+}
