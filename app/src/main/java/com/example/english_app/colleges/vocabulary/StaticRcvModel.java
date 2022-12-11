d27bcde02ae6b7c986158754aff540c9785c76a6
PhoebeJChang <phoebechang00@gmail.com>
2022-12-11 23:50:02 +0800

    yay

--- app/src/main/java/com/example/english_app/colleges/vocabulary/StaticRcvModel.java
+++ app/src/main/java/com/example/english_app/colleges/vocabulary/StaticRcvModel.java
@@ -3,11 +3,12 @@ package com.example.english_app.colleges.vocabulary;
 public class StaticRcvModel {
 
     private final int image;
-    private final String text;
+    private final String text, text2;
 
-    public StaticRcvModel(int image, String text) {
+    public StaticRcvModel(int image, String text, String text2) {
         this.image = image;
         this.text = text;
+        this.text2 = text2;
     }
 
     public int getImage() {
@@ -17,4 +18,6 @@ public class StaticRcvModel {
     public String getText() {
         return text;
     }
+
+    public String getText2() { return text2; }
 }
