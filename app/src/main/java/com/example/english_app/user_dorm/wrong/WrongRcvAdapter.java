afa273771b4e102e7d808cf6566ec9e9260109ee
PhoebeJChang <phoebechang00@gmail.com>
2022-12-15 11:05:17 +0800

    wrong recycler

--- app/src/main/java/com/example/english_app/user_dorm/wrong/WrongRcvAdapter.java
+++ app/src/main/java/com/example/english_app/user_dorm/wrong/WrongRcvAdapter.java
@@ -1,4 +1,4 @@
-package com.example.english_app.colleges.vocabulary;
+package com.example.english_app.user_dorm.wrong;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,37 +11,33 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.english_app.R;
+import com.example.english_app.colleges.vocabulary.VocabularyRcvAdapter;
+import com.example.english_app.colleges.vocabulary.VocabularyRcvModel;
 
 import java.util.ArrayList;
 
-public class VocabularyRcvAdapter extends RecyclerView.Adapter<VocabularyRcvAdapter.VocabularyRcvViewHolder> {
-
-    //list of vocabulary items
-    public ArrayList<VocabularyRcvModel> vocabularyList;
-    int check_StarPosition = -1; //沒有任何星星被選擇
+public class WrongRcvAdapter extends RecyclerView.Adapter<WrongRcvAdapter.WrongRcvViewHolder> {
+    public ArrayList<WrongRcvModel> wrongList;
+    int check_StarPosition = -1;
     private int star_click_ct = 0;
 
-    public VocabularyRcvAdapter(ArrayList<VocabularyRcvModel> vocabularyList) {
-        this.vocabularyList = vocabularyList;
+    public WrongRcvAdapter(ArrayList<WrongRcvModel> wrongList) {
+        this.wrongList = wrongList;
     }
 
-    public class VocabularyRcvViewHolder extends RecyclerView.ViewHolder {
+    public class WrongRcvViewHolder extends RecyclerView.ViewHolder {
         public TextView vocText, chText;
         LinearLayout linearLayout;
         private ImageButton imgBtnStar;
         //之後要補btn
 
-
-        public VocabularyRcvViewHolder(@NonNull View itemView) {
+        public WrongRcvViewHolder(@NonNull View itemView) {
             super(itemView);
+            //recycler
             vocText = itemView.findViewById(R.id.vocabulary);
             chText = itemView.findViewById(R.id.chinese);
             linearLayout = itemView.findViewById(R.id.vocabulary_rcv_linearlayout);
 //            imgBtnStar = itemView.findViewById(R.id.vocabulary_collection_btn);
-
-            //星星取得按下的position
-//            imgBtnStar.setOnClickListener(v -> checkPosition(getAbsoluteAdapterPosition()));
-
         }
 
         private void checkPosition(int adapterPosition) {
@@ -56,14 +52,14 @@ public class VocabularyRcvAdapter extends RecyclerView.Adapter<VocabularyRcvAdap
 
     @NonNull
     @Override
-    public VocabularyRcvAdapter.VocabularyRcvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public WrongRcvAdapter.WrongRcvViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.vocabulary_rcv_item, parent, false);
-        return new VocabularyRcvViewHolder(view);
+        return new WrongRcvViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull VocabularyRcvAdapter.VocabularyRcvViewHolder holder, int position) {
-        VocabularyRcvModel currentItem = vocabularyList.get(position);
+    public void onBindViewHolder(@NonNull WrongRcvAdapter.WrongRcvViewHolder holder, int position) {
+        WrongRcvModel currentItem = wrongList.get(position);
         holder.vocText.setText(currentItem.getVocText());
         holder.chText.setText(currentItem.getChText());
         //btn
@@ -76,9 +72,8 @@ public class VocabularyRcvAdapter extends RecyclerView.Adapter<VocabularyRcvAdap
 
     @Override
     public int getItemCount() {
-        return vocabularyList.size();
+        return wrongList.size();
     }
+
+
 }
-
-
-
