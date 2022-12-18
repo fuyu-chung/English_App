c361f7912f007e88d5dd7da987383c74cc169208
PhoebeJChang <phoebechang00@gmail.com>
2022-12-18 20:38:11 +0800

    yayy

--- app/src/main/java/com/example/english_app/AboutUsActivity.java
+++ app/src/main/java/com/example/english_app/AboutUsActivity.java
@@ -3,6 +3,7 @@ package com.example.english_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        ImageView backBtn = findViewById(R.id.absBackBtn);
+        backBtn.setOnClickListener(v -> onBackPressed());
     }
 }
