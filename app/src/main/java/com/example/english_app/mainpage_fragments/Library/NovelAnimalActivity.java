5598ab70a39b268c9de52e6e752aff2691b6c0c9
鍾馥羽 <fuyuchung@gmail.com>
2022-12-18 11:21:33 +0800

    novel url

--- app/src/main/java/com/example/english_app/mainpage_fragments/Library/NovelAnimalActivity.java
+++ app/src/main/java/com/example/english_app/mainpage_fragments/Library/NovelAnimalActivity.java
@@ -1,5 +1,7 @@
 package com.example.english_app.mainpage_fragments.Library;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -20,7 +22,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class NovelAnimalActivity extends AppCompatActivity implements CheckWhatNovelClickInterface{
-
+    ArrayList<String> url_list= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,6 +36,26 @@ public class NovelAnimalActivity extends AppCompatActivity implements CheckWhatN
 
         ImageView backBtn = findViewById(R.id.novBackBtn);
         backBtn.setOnClickListener(v -> onBackPressed());
+
+        ExecutorService executor = Executors.newSingleThreadExecutor(); // 建立新的thread
+        executor.execute(() -> {
+            try {
+                //String url = "";
+                String s1 = "jdbc:jtds:sqlserver://myenglishserver.database.windows.net:1433/englishapp_db;user=englishapp@myenglishserver;password=English1234@@;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request;"; //訪問azure的db的網址
+                Connection connection = DriverManager.getConnection(s1); //建立連線
+                String query = "select Url from novel where Category = 'Animal'";
+                PreparedStatement statement = connection.prepareStatement(query);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    //url = resultSet.getString(1);
+                    url_list.add(resultSet.getString(1));
+                }
+                System.out.println(url_list);
+                executor.shutdown();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private ArrayList<NovelRcvModel> getListNovel() {
@@ -67,6 +89,9 @@ public class NovelAnimalActivity extends AppCompatActivity implements CheckWhatN
 
     @Override
     public void onNovelTitleClicked(int position) {
-
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = url_list.get(position);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/english_app/mainpage_fragments/Library/NovelChildActivity.java
+++ app/src/main/java/com/example/english_app/mainpage_fragments/Library/NovelChildActivity.java
@@ -1,5 +1,7 @@
 package com.example.english_app.mainpage_fragments.Library;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -20,6 +22,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class NovelChildActivity extends AppCompatActivity implements CheckWhatNovelClickInterface{
+    ArrayList<String> url_list= new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +35,28 @@ public class NovelChildActivity extends AppCompatActivity implements CheckWhatNo
         rcvNovel.setAdapter(novelRcvAdapter);
         rcvNovel.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
 
-
         ImageView backBtn = findViewById(R.id.novBackBtn);
         backBtn.setOnClickListener(v -> onBackPressed());
+
+        ExecutorService executor = Executors.newSingleThreadExecutor(); // 建立新的thread
+        executor.execute(() -> {
+            try {
+                //String url = "";
+                String s1 = "jdbc:jtds:sqlserver://myenglishserver.database.windows.net:1433/englishapp_db;user=englishapp@myenglishserver;password=English1234@@;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;ssl=request;"; //訪問azure的db的網址
+                Connection connection = DriverManager.getConnection(s1); //建立連線
+                String query = "select Url from novel where Category = 'Children'";
+                PreparedStatement statement = connection.prepareStatement(query);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    //url = resultSet.getString(1);
+                    url_list.add(resultSet.getString(1));
+                }
+                System.out.println(url_list);
+                executor.shutdown();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
     private ArrayList<NovelRcvModel> getListNovel() {
@@ -68,6 +90,9 @@ public class NovelChildActivity extends AppCompatActivity implements CheckWhatNo
 
     @Override
     public void onNovelTitleClicked(int position) {
-
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        String url = url_list.get(position);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
